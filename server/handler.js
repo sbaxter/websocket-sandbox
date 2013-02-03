c47c336db133b722a054bddbded6359f1b0fc86f
Seth Baxter <me@sethbaxter.com>
2013-02-03 16:40:04 -0500

    adds mime module

--- server/handler.js
+++ server/handler.js
@@ -1,19 +1,22 @@
 module.exports = function ( request, response ) {
   var fs = require( 'fs-extra' )
     , dir = __dirname + '/../client/'
-    , file = request.url.split( '?' )[0];
+    , file = request.url.split( '?' )[0]
+    , mime = require( 'mime' );
 
   file = ( file === '/' || file === '' ) ? 'index.html' : file;
   dir = ( file.indexOf( 'components' ) !== -1 ) ? __dirname + '/../' : dir;
 
   // Read File
   fs.readFile( dir + file, function ( error, data ) {
+    var mimeType = mime.lookup( file );
+
     if ( error ) {
       response.writeHead( 500 );
       return response.end( 'Error loading ' + file );
     }
 
-    response.writeHead( 200 );
+    response.writeHead( 200, { 'Content-Type' : mimeType } );
     response.end( data );
   });
 };
